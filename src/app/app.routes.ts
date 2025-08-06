c76cf9b2344019fbee9f539c5ace4adf07180f48
Will_Moura <william@febrafar.com.br>
2025-08-06 15:40:54 -0300

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListExpensesComponent } from './list-expenses/list-expenses.component';
+import { EnglishTeacherComponent } from './english-teacher/english-teacher.component';
 
 export const routes: Routes = [
-    { path: 'list-expenses', component: ListExpensesComponent }
+    { path: 'list-expenses', component: ListExpensesComponent },
+    { path: 'teacher', component: EnglishTeacherComponent}
 ];
